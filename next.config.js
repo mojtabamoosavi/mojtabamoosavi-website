f77fbc832d60d93bbd5d98bcd8a46f17d9edc01a
Mojtaba Moosavi <info.mojtabamoosavi@gmail.com>
2022-11-12 15:43:10 +0330

    Change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     experimental: {
         appDir: true,
+    },
+    assetPrefix: isProd ? "/mojtabamoosavi-website/" : "",
+    images: {
+        unoptimized: true,
     },
 }
 
