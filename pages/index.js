883de683fffab6a9a21ada99cfb977adf959a3e0
Mojtaba Moosavi <info.mojtabamoosavi@gmail.com>
2022-11-13 12:18:16 +0330

    Add avatar to home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
+import Image from "next/image";
 import Layout from "../components/layout/Layout";
+import avatar from '../public/avatar.jpg'
 
 export default function Home() {
     return (
         <Layout>
-            <div className="flex flex-1 justify-center items-center text-center">
-                <div>
+            <div className="flex flex-1 justify-center items-center">
+                <div className="flex flex-col items-center">
+                    <Image src={avatar} alt="avatar" className="mb-6 rounded-full" />
                     <h1 className="text-4xl font-bold">Mojtaba Moosavi</h1>
-                    <h2 className="text-2xl text-gray-400 mt-2">Front-End Developer</h2>
+                    <h2 className="text-2xl text-gray-400 mt-3">Front-End Developer</h2>
                 </div>
             </div>
         </Layout>
