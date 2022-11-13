deed8d417ecce07e62876e5e82dfb8e803a94f95
Mojtaba Moosavi <info.mojtabamoosavi@gmail.com>
2022-11-13 12:18:04 +0330

    Change footer color

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="py-4 text-sm">
+        <footer className="py-4 text-sm text-gray-400">
             <div className="container">
                 © 2022 · Powered by <a href="https://nextjs.org">Next.js</a> & <a href="https://github.com">Github Pages</a>.
             </div>
